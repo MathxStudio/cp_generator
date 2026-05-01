f678492b7cce1c604c07d328233d2a8c6a0e6b89
Allan Pan <mathxstudio@gmail.com>
2026-05-01 14:50:05 +0800

    Fix Android pointer import

--- android-app/app/src/main/java/com/mathxstudio/cpgenerator/MainActivity.kt
+++ android-app/app/src/main/java/com/mathxstudio/cpgenerator/MainActivity.kt
@@ -53,7 +53,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
